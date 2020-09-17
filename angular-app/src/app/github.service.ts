02f9203b0d5e6cd63a877c464702b2810829cb2a
Silzhar <luis.ruizfer@gmail.com>
2020-09-17 11:39:53 +0200

    Filtrado de entrada a necesidades de la interface

--- angular-app/src/app/github.service.ts
+++ angular-app/src/app/github.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { strict } from 'assert';
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +14,25 @@ export class GithubService {
     public peticionRepositorios(username: string): Observable<any>{
         const ruta: string = `https://api.github.com/users/${username}/repos `;
         // Observable .
-        return this.http.get(ruta);
+        const resultObservable = this.http.get(ruta);
+        const observableMap = resultObservable.pipe(map((data: any[]) => {
+            const repositories: Repo[] = [];
+            data.forEach((element) => {
+                const elementMap: Repo = {
+                    name: element.name,
+                    url: element.url,
+                    description: element.description,
+                };
+                repositories.push(elementMap);
+            });
+            return repositories;
+        }));
+        return observableMap;
     }
+}
+
+export interface Repo {
+    name: string;
+    url: string;
+    description: string;
 }
